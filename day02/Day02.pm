9f79fea94c3cd5aeccb5f45e5d10fe85429c9a1c
Greg Hosking <hoskinggregory@gmail.com>
2024-12-04 09:58:27 -0500

    Day 2 part 2

--- day02/Day02.pm
+++ day02/Day02.pm
@@ -42,8 +42,57 @@ sub part1 {
     return $iSafe;
 }
 
-# sub part2 {
+sub part2 {
+    my ($self, $sInput) = @_;
 
-# }
+    my $iSafe = 0;
+    foreach my $sLine (split(/\n/, $sInput)) {
+        my @aLevels = split(/\s+/, $sLine);
+        my @aaLevelsDamp = [@aLevels];
+        for (my $ii = 0; $ii < scalar(@aLevels); $ii++) {
+            my @aTemp = @aLevels;
+            splice(@aTemp, $ii, 1);
+            push(@aaLevelsDamp, [@aTemp]);
+        }
+
+        my @aaDiffs;
+        foreach my $paLevelsDamp (@aaLevelsDamp) {
+            my @aDiffs;
+            for (my $ii = 0; $ii < scalar(@$paLevelsDamp) - 1; $ii++) {
+                push(@aDiffs, @$paLevelsDamp[$ii + 1] - @$paLevelsDamp[$ii]);
+            }
+            push(@aaDiffs, [@aDiffs]);
+        }
+
+        foreach my $paDiffs (@aaDiffs) {
+            my $bSafe = 1;
+            my $iSign = 0;
+            foreach my $iDiff (@$paDiffs) {
+                if (!$iDiff) {
+                    $bSafe = 0;
+                    last;
+                }
+                if (!$iSign) {
+                    $iSign = 1 if ($iDiff > 0);
+                    $iSign = -1 if ($iDiff < 0);
+                }
+                if (($iDiff > 0 && $iSign == -1) || ($iDiff < 0 && $iSign == 1)) {
+                    $bSafe = 0;
+                    last;
+                }
+                if (abs($iDiff) < 1 || abs($iDiff) > 3) {
+                    $bSafe = 0;
+                    last;
+                }
+            }
+            if ($bSafe) {
+                $iSafe++;
+                last;
+            }
+        }
+    }
+
+    return $iSafe;
+}
 
 1;
