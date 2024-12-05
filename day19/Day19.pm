3c75e018fe29a41e284ea5976b9ca8fb125f9528
Greg Hosking <hoskinggregory@gmail.com>
2024-12-04 22:28:56 -0500

    Escape @_ in day Perl template created by setup_day.pl & re-run setup_all_days.pl

--- day19/Day19.pm
+++ day19/Day19.pm
@@ -6,12 +6,12 @@ use lib 'C:\advent_of_code_2024';
 use parent 'Day';
 
 # sub part1 {
-#     my ($self, $sInput) = ;
+#     my ($self, $sInput) = @_;
 #     # Solution...
 # }
 
 # sub part2 {
-#     my ($self, $sInput) = ;
+#     my ($self, $sInput) = @_;
 #     # Solution...
 # }
 
