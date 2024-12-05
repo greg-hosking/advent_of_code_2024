65ec156c875dd36368433aa2d7eea96e22796d6c
Greg Hosking <hoskinggregory@gmail.com>
2024-12-04 22:00:41 -0500

    Solve day 3 part 1

--- day03/Day03.pm
+++ day03/Day03.pm
@@ -6,7 +6,15 @@ use lib 'C:\advent_of_code_2024';
 use parent 'Day';
 
 sub part1 {
-    return -1;
+    my ($self, $sInput) = @_;
+    my @aMatches = $sInput =~ m/mul\(\d+,\d+\)/g;
+    my $iSum = 0;
+    foreach my $sMatch (@aMatches) {
+        if ($sMatch =~ m/mul\((\d+),(\d+)\)/) {
+            $iSum += ($1 * $2);
+        }
+    }
+    return $iSum;
 }
 
 # sub part2 {
