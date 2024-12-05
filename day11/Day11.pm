a1ac8e8f74c58d327d2b4c0dd8c6883ef56e86b2
Greg Hosking <hoskinggregory@gmail.com>
2024-12-04 22:00:20 -0500

    Ran setup_all_days.pl...

---
+++ day11/Day11.pm
@@ -0,0 +1,18 @@
+package Day11;
+
+use strict;
+use warnings;
+use lib 'C:\advent_of_code_2024';
+use parent 'Day';
+
+# sub part1 {
+#     my ($self, $sInput) = ;
+#     # Solution...
+# }
+
+# sub part2 {
+#     my ($self, $sInput) = ;
+#     # Solution...
+# }
+
+1;
