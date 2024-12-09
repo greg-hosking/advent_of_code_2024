ee485e64df5d81627f828d073f82162e2310020f
Greg Hosking <hoskinggregory@gmail.com>
2024-12-09 15:48:11 -0500

    Tidy up day 7 per Brian's helpful suggestion

--- day07/Day07.pm
+++ day07/Day07.pm
@@ -8,53 +8,18 @@ use parent 'Day';
 sub part1 {
     my ($self, $sInput) = @_;
     my @aOps = qw(+ *);
-    my %hOpCombosPerLength;
-    my $iSum = 0;
-    foreach my $sLine (split(/\n/, $sInput)) {
-        my ($iTargetVal, $sNums) = split(/: /, $sLine);
-        my @aNums = split(/ /, $sNums);
-        my $iNumOps  = scalar(@aNums) - 1;
-        my %hOpCombos;
-        if (exists $hOpCombosPerLength{$iNumOps}) {
-            %hOpCombos = %{$hOpCombosPerLength{$iNumOps}};
-        } else {
-            my $iNumCombos = 0;
-            while ($iNumCombos <= scalar(@aOps) ** $iNumOps) {
-                my $sCombo = "";
-                for (my $ii = 0; $ii < $iNumOps; $ii++) {
-                    $sCombo .= $aOps[int(rand(scalar(@aOps)))];
-                }
-                if (!exists $hOpCombos{$sCombo}) {
-                    $hOpCombos{$sCombo} = 1;
-                    $iNumCombos++;
-                }
-                last if ($iNumCombos == scalar(@aOps) ** $iNumOps);
-            }
-            $hOpCombosPerLength{$iNumOps} = \%hOpCombos;
-        }
-        foreach my $sCombo (keys %hOpCombos) {
-            my @aCombo = split(//, $sCombo);
-            my $iVal = $aNums[0];
-            for (my $ii = 0; $ii < scalar(@aCombo); $ii++) {
-                if ($aCombo[$ii] eq "+") {
-                    $iVal += $aNums[$ii + 1]; 
-                } else {
-                    $iVal *= $aNums[$ii + 1];
-                }
-            }
-            if ($iVal == $iTargetVal) {
-                $iSum += $iVal;
-                last;
-            }
-        }
-    }
-
-    return $iSum;
+    return partN($self, $sInput, \@aOps);
 }
 
 sub part2 {
     my ($self, $sInput) = @_;
     my @aOps = qw(+ * |);
+    return partN($self, $sInput, \@aOps);
+}
+
+sub partN {
+    my ($self, $sInput, $paOps) = @_;
+    my @aOps = @$paOps;
     my %hOpCombosPerLength;
     my $iSum = 0;
     foreach my $sLine (split(/\n/, $sInput)) {
