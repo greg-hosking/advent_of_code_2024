3d38b20de6b07a31fea8101e8072dbb0299c403a
Greg Hosking <hoskinggregory@gmail.com>
2024-12-10 23:18:43 -0500

    Begin day 9

--- day09/Day09.pm
+++ day09/Day09.pm
@@ -5,10 +5,28 @@ use warnings;
 use lib 'C:\advent_of_code_2024';
 use parent 'Day';
 
-# sub part1 {
-#     my ($self, $sInput) = @_;
-#     # Solution...
-# }
+use Data::Dumper;
+
+sub part1 {
+    my ($self, $sInput) = @_;
+
+    my @aDisk;
+    my @aDigits = split(//, $sInput);
+    for (my $ii = 0; $ii < scalar(@aDigits); $ii++) {
+        my $iId = ($ii % 2 == 0) ? $ii / 2 : ".";
+        for (my $ij = 0; $ij < $aDigits[$ii]; $ij++) {
+            push(@aDisk, $iId);
+        }
+    }
+
+    # TODO: what if "file" size is 0...?
+
+    print "Input: $sInput\n";
+    print "Disk:  " . join("", @aDisk) . "\n";
+
+    return -1;
+}
+
 
 # sub part2 {
 #     my ($self, $sInput) = @_;
