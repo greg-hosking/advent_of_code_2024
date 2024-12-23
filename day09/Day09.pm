16a73b3b0d4c4a018be78349b3fae83fb613320e
Greg Hosking <hoskinggregory@gmail.com>
2024-12-23 00:10:05 -0500

    Solve day 9 part 2 very inefficiently

--- day09/Day09.pm
+++ day09/Day09.pm
@@ -9,6 +9,7 @@ use Data::Dumper;
 
 sub part1 {
     my ($self, $sInput) = @_;
+    chomp $sInput;
 
     my @aDisk;
     my @aDigits = split(//, $sInput);
@@ -43,24 +44,52 @@ sub part1 {
     return $iChecksum;
 }
 
-
 sub part2 {
     my ($self, $sInput) = @_;
+    chomp $sInput;
 
     my @aDisk;
     my @aDigits = split(//, $sInput);
+    my $iMaxId = -1;
     for (my $ii = 0; $ii < scalar(@aDigits); $ii++) {
         my $iId = ($ii % 2 == 0) ? $ii / 2 : ".";
+        $iMaxId = $iId if ($iId ne "." && $iId > $iMaxId);
         for (my $ij = 0; $ij < $aDigits[$ii]; $ij++) {
             push(@aDisk, $iId);
         }
     }
 
-    # TODO...
+    my $iRight = scalar(@aDisk);
+    my $iId = $iMaxId;
+    while ($iId > -1) {
+        my @aFile;
+        for (my $ii = 0; $ii < $iRight; $ii++) {
+            push(@aFile, $ii) if ($aDisk[$ii] eq $iId);
+        }        
+        my @aaFree;
+        for (my $ii = 0; $ii < $iRight; $ii++) {
+            last if ($ii > $aFile[0]);
+            next if ($aDisk[$ii] ne ".");
+            my @aFree;
+            while ($ii < $iRight) {
+                last if ($aDisk[$ii] ne ".");
+                push(@aFree, $ii);
+                $ii++;    
+            }
+            next if (scalar(@aFree) < scalar(@aFile));
+            for (my $ii = 0; $ii < scalar(@aFile); $ii++) {
+                $aDisk[$aFree[$ii]] = $iId;
+                $aDisk[$aFile[$ii]] = ".";
+            }
+            last;
+        }
+        $iRight = $aFile[0];
+        $iId--;
+    }
 
     my $iChecksum = 0;
     for (my $ii = 0; $ii < scalar(@aDisk); $ii++) {
-        last if ($aDisk[$ii] eq ".");
+        next if ($aDisk[$ii] eq ".");
         $iChecksum += $ii * $aDisk[$ii];
     }
 
