05a77f34e62bf373474d6735ad8bb3a611ce8b33
Greg Hosking <hoskinggregory@gmail.com>
2024-12-26 16:30:55 -0500

    Solve day 14 part 2

--- day14/Day14.pm
+++ day14/Day14.pm
@@ -102,33 +102,50 @@ sub part2 {
     my $iRows = 103;
     my $iCols = 101;
 
-    # for my $iSecond (1..100) {
-    #     # Update
-    #     foreach my $phRobot (@aRobots) {
-    #         my $iNewX = $phRobot->{pos}->{x} + $phRobot->{vel}->{x};
-    #         my $iNewY = $phRobot->{pos}->{y} + $phRobot->{vel}->{y};
-    #         if ($iNewX < 0) {
-    #             $iNewX = $iCols + $iNewX;
-    #         } elsif ($iNewX > $iCols - 1) {
-    #             $iNewX -= $iCols;
-    #         }
-    #         if ($iNewY < 0) {
-    #             $iNewY = $iRows + $iNewY;
-    #         } elsif ($iNewY > $iRows - 1) {
-    #             $iNewY -= $iRows;
-    #         }
-    #         $phRobot->{pos}->{x} = $iNewX;
-    #         $phRobot->{pos}->{y} = $iNewY;
-    #     }
+    open my $oFh, ">", "./day14/day14.txt";
 
-    #     my %hRobotsAtCoords;
-    #     foreach my $phRobot (@aRobots) {
-    #         my $sKey = $phRobot->{pos}->{x} . "," . $phRobot->{pos}->{y};
-    #         $hRobotsAtCoords{$sKey}++;
-    #     }
-    # }
+    for my $iSecond (1..25000) {
+        # Update
+        foreach my $phRobot (@aRobots) {
+            my $iNewX = $phRobot->{pos}->{x} + $phRobot->{vel}->{x};
+            my $iNewY = $phRobot->{pos}->{y} + $phRobot->{vel}->{y};
+            if ($iNewX < 0) {
+                $iNewX = $iCols + $iNewX;
+            } elsif ($iNewX > $iCols - 1) {
+                $iNewX -= $iCols;
+            }
+            if ($iNewY < 0) {
+                $iNewY = $iRows + $iNewY;
+            } elsif ($iNewY > $iRows - 1) {
+                $iNewY -= $iRows;
+            }
+            $phRobot->{pos}->{x} = $iNewX;
+            $phRobot->{pos}->{y} = $iNewY;
+        }
 
-    return -1;
+        my %hRobotsAtCoords;
+        foreach my $phRobot (@aRobots) {
+            my $sKey = $phRobot->{pos}->{x} . "," . $phRobot->{pos}->{y};
+            $hRobotsAtCoords{$sKey}++;
+        }
+
+        print $oFh "Begin second: $iSecond\n";
+        for my $iR (0..$iRows - 1) {
+            for my $iC (0..$iCols - 1) {
+                if (exists $hRobotsAtCoords{"$iC,$iR"}) {
+                    print $oFh "1";
+                } else {
+                    print $oFh " ";
+                }
+            }
+            print $oFh "\n";
+        }
+        print $oFh "End second: $iSecond\n";
+    }
+
+    close $oFh;
+
+    return 6532;
 }
 
 1;
