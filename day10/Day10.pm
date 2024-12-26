a5853ab9fb3345c2e5e2513a39550d7d9680aa1d
Greg Hosking <hoskinggregory@gmail.com>
2024-12-26 09:13:00 -0500

    Start day 10 and 13

--- day10/Day10.pm
+++ day10/Day10.pm
@@ -5,10 +5,45 @@ use warnings;
 use lib 'C:\advent_of_code_2024';
 use parent 'Day';
 
-# sub part1 {
-#     my ($self, $sInput) = @_;
-#     # Solution...
-# }
+sub try_hike {
+    my ($iPrevHeight, $iRow, $iCol, $paaGrid, $phVisited) = @_;
+
+    my $iRows = scalar(@$paaGrid);
+    my $iCols = scalar(@{$paaGrid->[0]});
+    return 0 if ($iRow < 0 || $iRow >= $iRows || $iCol < 0 || $iCol >= $iCols);
+    return 0 if (exists $phVisited->{"$iRow,$iCol"});
+    $phVisited->{"$iRow,$iCol"} = 1;
+
+    my $iHeight = $paaGrid->[$iRow]->[$iCol]; 
+    return 0 if ($iHeight != $iPrevHeight + 1);
+    return 1 if ($iPrevHeight == 8 && $iHeight == 9);
+
+    return try_hike($iHeight, $iRow - 1, $iCol, $paaGrid, $phVisited) + try_hike($iHeight, $iRow + 1, $iCol, $paaGrid, $phVisited) + 
+           try_hike($iHeight, $iRow, $iCol - 1, $paaGrid, $phVisited) + try_hike($iHeight, $iRow, $iCol + 1, $paaGrid, $phVisited); 
+}
+
+sub part1 {
+    my ($self, $sInput) = @_;
+
+    my @aaGrid;
+    foreach my $sRow (split(/\n/, $sInput)) {
+        push(@aaGrid, [split(//, $sRow)]);
+    }
+
+    my $iRows = scalar(@aaGrid);
+    my $iCols = scalar(@{$aaGrid[0]});
+    my $iSum = 0;
+    for my $iR (0..$iRows - 1) {
+        for my $iC (0..$iCols - 1) {
+            if ($aaGrid[$iR]->[$iC] == 0) {
+                my %hVisited = ();
+                $iSum += try_hike(-1, $iR, $iC, \@aaGrid, \%hVisited);
+            }
+        }
+    }
+
+    return $iSum;
+}
 
 # sub part2 {
 #     my ($self, $sInput) = @_;
--- day13/Day13.pm
+++ day13/Day13.pm
@@ -5,14 +5,91 @@ use warnings;
 use lib 'C:\advent_of_code_2024';
 use parent 'Day';
 
-# sub part1 {
-#     my ($self, $sInput) = @_;
-#     # Solution...
-# }
+use Data::Dumper;
+
+sub part1 {
+    my ($self, $sInput) = @_;
+    chomp $sInput;
+
+    my @aLines = split(/\n/, $sInput);
+    my @aMachines;
+    for (my $ii = 0; $ii < scalar(@aLines); $ii += 4) {
+        my %hMachine;
+        if ($aLines[$ii] =~ /Button A: X\+(.*), Y\+(.*)/) {
+            $hMachine{a}{x} = $1;
+            $hMachine{a}{y} = $2;            
+        }
+        if ($aLines[$ii + 1] =~ /Button B: X\+(.*), Y\+(.*)/) {
+            $hMachine{b}{x} = $1;
+            $hMachine{b}{y} = $2;
+        }
+        if ($aLines[$ii + 2] =~ /Prize: X=(.*), Y=(.*)/) {
+            $hMachine{prize}{x} = $1;
+            $hMachine{prize}{y} = $2;
+        }
+        push(@aMachines, \%hMachine);
+    }
+    
+    my $iTokens = 0;
+    foreach my $phMachine (@aMachines) {
+        my $bPossible = 0;
+        for my $iA (0..100) {
+            for my $iB (0..100) {
+                my $iX = ($phMachine->{a}->{x} * $iA) + ($phMachine->{b}->{x} * $iB);
+                my $iY = ($phMachine->{a}->{y} * $iA) + ($phMachine->{b}->{y} * $iB);
+                if ($iX == $phMachine->{prize}->{x} && $iY == $phMachine->{prize}->{y}) {
+                    $iTokens += (3 * $iA) + $iB;
+                    $bPossible = 1;
+                    last;
+                }
+            }
+            last if ($bPossible);
+        }
+    }
+
+    return $iTokens;
+}
 
 # sub part2 {
 #     my ($self, $sInput) = @_;
-#     # Solution...
-# }
+#     chomp $sInput;
 
+#     my @aLines = split(/\n/, $sInput);
+#     my @aMachines;
+#     for (my $ii = 0; $ii < scalar(@aLines); $ii += 4) {
+#         my %hMachine;
+#         if ($aLines[$ii] =~ /Button A: X\+(.*), Y\+(.*)/) {
+#             $hMachine{a}{x} = $1;
+#             $hMachine{a}{y} = $2;            
+#         }
+#         if ($aLines[$ii + 1] =~ /Button B: X\+(.*), Y\+(.*)/) {
+#             $hMachine{b}{x} = $1;
+#             $hMachine{b}{y} = $2;
+#         }
+#         if ($aLines[$ii + 2] =~ /Prize: X=(.*), Y=(.*)/) {
+#             $hMachine{prize}{x} = $1 + 10000000000000;
+#             $hMachine{prize}{y} = $2 + 10000000000000;
+#         }
+#         push(@aMachines, \%hMachine);
+#     }
+    
+#     my $iTokens = 0;
+#     foreach my $phMachine (@aMachines) {
+#         my $bPossible = 0;
+#         for my $iA (reverse 10000..1000000) {
+#             for my $iB (reverse 10000..1000000) {
+#                 my $iX = ($phMachine->{a}->{x} * $iA) + ($phMachine->{b}->{x} * $iB);
+#                 my $iY = ($phMachine->{a}->{y} * $iA) + ($phMachine->{b}->{y} * $iB);
+#                 if ($iX == $phMachine->{prize}->{x} && $iY == $phMachine->{prize}->{y}) {
+#                     $iTokens += (3 * $iA) + $iB;
+#                     $bPossible = 1;
+#                     last;
+#                 }
+#             }
+#             last if ($bPossible);
+#         }
+#     }
+
+#     return $iTokens;
+# }
 1;
