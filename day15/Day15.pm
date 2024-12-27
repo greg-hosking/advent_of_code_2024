f07b8d399c798eaa35fa070199ce7240a1d443c8
Greg Hosking <hoskinggregory@gmail.com>
2024-12-27 17:35:07 -0500

    Solve day 15 part 1

--- day15/Day15.pm
+++ day15/Day15.pm
@@ -30,16 +30,60 @@ sub part1 {
         }
     }
 
-    # print Dumper(\@aaGrid);
-    # print Dumper(\@aMovements);
-
-    foreach my $sMove (@aMovements) {
-        my ($iDx, $iDy) = translate_move($sMove);
-        print "$iDx, $iDy\n";
+    my %hRobot;
+    for (my $ii = 0; $ii < scalar(@aaGrid); $ii++) {
+        for (my $ij = 0; $ij < scalar(@{$aaGrid[$ii]}); $ij++) {
+            if ($aaGrid[$ii]->[$ij] eq "@") {
+                $hRobot{r} = $ii;
+                $hRobot{c} = $ij;
+            }
+        }
     }
 
-    return -1;
+    foreach my $sMove (@aMovements) {
+        my ($iDR, $iDC) = translate_move($sMove);
+        my $sTarget = $aaGrid[$hRobot{r} + $iDR]->[$hRobot{c} + $iDC];
+        next if ($sTarget eq "#");
+        if ($sTarget eq ".") {
+            $aaGrid[$hRobot{r}]->[$hRobot{c}] = ".";
+            $hRobot{r} += $iDR;
+            $hRobot{c} += $iDC;
+            $aaGrid[$hRobot{r}]->[$hRobot{c}] = "@";
+        } else {
+            # Look for an empty space...
+            my $iR = $hRobot{r} + $iDR;
+            my $iC = $hRobot{c} + $iDC;
+            my $bCanMove = 0;
+            while (1) {
+                $iR += $iDR;
+                $iC += $iDC;
+                $sTarget = $aaGrid[$iR]->[$iC];
+                last if ($sTarget eq "#");
+                if ($sTarget eq ".") {
+                    $bCanMove = 1;
+                    last;
+                }
+            }
+            next if (!$bCanMove);
+            while ($iR != $hRobot{r} || $iC != $hRobot{c}) {
+                $aaGrid[$iR]->[$iC] = $aaGrid[$iR - $iDR]->[$iC - $iDC];
+                $iR -= $iDR;
+                $iC -= $iDC;
+            }
+            $aaGrid[$iR]->[$iC] = ".";
+            $hRobot{r} = $iR + $iDR;
+            $hRobot{c} = $iC + $iDC;
+        }
+    }
 
+    my $iSum = 0;
+    for (my $ii = 0; $ii < scalar(@aaGrid); $ii++) {
+        for (my $ij = 0; $ij < scalar(@{$aaGrid[$ii]}); $ij++) {
+            $iSum += (100 * $ii) + $ij if ($aaGrid[$ii]->[$ij] eq "O");
+        }
+    }
+
+    return $iSum;
 }
 
 # sub part2 {
